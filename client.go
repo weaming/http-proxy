f944a2a4e684bae60b3261ab0b29ff6edb643159
weaming <garden.yuen@gmail.com>
2018-12-19 23:01:03 -0500

    add whoyouare

--- client.go
+++ client.go
@@ -5,15 +5,29 @@ import (
 	"log"
 	"bytes"
 	"strings"
+	"encoding/json"
 	"github.com/smartystreets/cproxy"
 )
 
 var explicitForwardProxyHandler = cproxy.Configure()
 
+func whoYouAre(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	body, _ := json.Marshal(map[string]string{
+		"addr": r.RemoteAddr,
+	})
+	w.Write(body)
+}
+
 func forwardServer(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "CONNECT" {
 		//w.WriteHeader(200)
 		explicitForwardProxyHandler.ServeHTTP(w, r)
+		return
+	}
+
+	if r.URL.Path == "/whoyouare" {
+		http.HandlerFunc(whoYouAre).ServeHTTP(w, r)
 		return
 	}
 
