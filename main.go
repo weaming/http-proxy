898c0c0e83b90093fd36fa22da11ce8d856f5216
weaming <garden.yuen@gmail.com>
2018-12-20 14:02:39 -0500

    update cli

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 )
 
-var listen = flag.String("listen", ":10008", "linsten on")
-var server = flag.String("server", "", "server host:port")
+var listen = flag.String("listen", ":8123", "listen host:port")
+var server = flag.String("server", "", "server host:port, run as client if given")
 var simple = flag.Bool("simple", false, "serve http proxy without client")
 
 func init() {
