2cf3adc4ca4ad0527b9a7f055aed5850f1bf0fa4
weaming <garden.yuen@gmail.com>
2018-12-20 23:35:20 -0500

    add  flag for proxychain

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ var listen = flag.String("listen", ":8123", "listen host:port")
 var server = flag.String("server", "", "server host:port, run as client if given")
 var simple = flag.Bool("simple", false, "serve http proxy without client")
 
+// proxy chain
+var httpProxy = flag.String("http", "", "http proxy host:port")
+var httpsProxy = flag.String("https", "", "https proxy host:port")
+var socksV4Proxy = flag.String("socks4", "", "socks4 proxy host:port")
+var socksV5Proxy = flag.String("socks5", "", "socks5 proxy host:port")
+
 func init() {
 	flag.Parse()
 }
